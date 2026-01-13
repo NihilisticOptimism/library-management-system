9a1720ea178fd433933c046418dace2ad93aa3f6
Red-Comm <Yan22tank@yandex.ru>
2026-01-13 18:23:42 +0300

    Add Library class structure

--- src/main/java/library/Library.java
+++ src/main/java/library/Library.java
@@ -1,3 +1,25 @@
+import java.util.List;
+import java.time.LocalDateTime;
+
 public class Library {
-  
+    private List<Book> books;
+    private OperationLog operationLog;
+
+    // Вложенный статический класс
+    public static class OperationLog {
+        // Внутренний класс для записи операции
+        public class LogEntry {
+            private OperationType type;
+            private LocalDateTime timestamp;
+            private String description;
+            // конструктор, геттеры, toString()
+        }
+
+        public enum OperationType {
+            ADD_BOOK, BORROW, RETURN
+        }
+
+        private List<LogEntry> entries;
+        // методы: addEntry(), getEntries(), printLog()
+    }
 }
