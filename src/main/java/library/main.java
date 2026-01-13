288a270ef467173fff9eb3c7bd8a2fc0140c4842
Red-Comm <Yan22tank@yandex.ru>
2026-01-13 18:37:19 +0300

    Add demo scenario with sample books

--- src/main/java/library/main.java
+++ src/main/java/library/main.java
@@ -1,6 +1,19 @@
 public class main {
     public static void main(String[] args) {
         Library library = new Library();
-        
+        library.addBook(new Book(1, "War and Peace",
+                "Lev Tolstoy", 1869, "978-5-17-090335-2"));
+        library.addBook(new Book(2, "Crime and Punish,e",
+                "Fyodor Dostoevsky", 1866, "978-5-17-090336-9"));
+        library.addBook(new Book(3, "Anna Karenina",
+                "Lev Tolstoy", 1877, "978-5-17-090337-6"));
+
+        System.out.println(library.findBookById(1).getTitle());
+        for(Book book : library.getAvailableBooks()) {
+            System.out.println(book.toString());
+        }
+        library.borrowBook(2);
+        library.returnBook(2);
+        library.printOperationLog();
     }
 }
