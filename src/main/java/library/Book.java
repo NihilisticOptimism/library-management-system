50c60ba399605678bf1a76cad52cce7e5a7bb9e5
Red-Comm <Yan22tank@yandex.ru>
2026-01-13 18:20:49 +0300

    Add Book class with basic fields and methods

--- src/main/java/library/Book.java
+++ src/main/java/library/Book.java
@@ -1,3 +1,54 @@
 public class Book {
 
+    private int id;
+    private String title;
+    private String author;
+    private int year;
+    private String isbn;
+    private boolean available;
+
+    public Book (int id, String title, String author, int year, String isbn) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.isbn = isbn;
+        this.setAvailable(true);
+    }
+
+    public void setAvailable(boolean is_available) {
+        this.available = is_available;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public boolean getAvailable() {
+        return available;
+    }
+
+    public String getStatus() {
+        return available ? "Free" : "Taken";
+    }
+
+    public String toString() {
+        return String.format("[ID: %d] \"%s\" -  %s (%d)\nISBN: %s | Status: %s", id, title, author, year, isbn, getStatus());
+    }
 }
